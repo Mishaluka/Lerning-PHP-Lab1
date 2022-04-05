ab9a58e80b257cf5143216c2ca812e52462397c9
Mikhail <83475413+Mishaluka@users.noreply.github.com>
2022-04-04 21:00:21 -0400

    Add files via upload

---
+++ phpLab1.php
@@ -0,0 +1,26 @@
+
+
+<?php
+//Q1: Write a PHP program to print your name and age to the browser.
+
+echo "Mikhail Lukianov" ."<br>";
+echo "I'm 32 years old" ."<br><br>";
+
+
+//Q2: Write a PHP program which creates two variables that store your name and age and print them to the browser. 
+$name ="Mikhail" ."<br>";
+$age ="32" ."<br><br>";
+echo "My name is " .$name;
+echo "I am " .$age;
+echo "<br>";
+
+//Q3: Write a PHP program which creates constant variable that store your college name and print the value to the browser. 
+define("greeting", "LaGuardia College");
+echo greeting;
+echo "<br><br>";
+
+//Q4: Write a PHP program which creates the following variables and print them to the browser.
+$x=10;
+$y=7;
+echo $x, " and " .$y;
+?>
